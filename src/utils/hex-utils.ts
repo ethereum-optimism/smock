a5975378035f32e0989ee7356f6d1d7c08a2cab6
smartcontracts <kelvinfichter@gmail.com>
2021-04-07 19:06:13 -0700

    fix: Make smoddit work for bytesN map keys (#36)

--- src/utils/hex-utils.ts
+++ src/utils/hex-utils.ts
@@ -6,10 +6,15 @@ export const toHexString32 = (
   value: string | number | BigNumber | boolean
 ): string => {
   if (typeof value === 'string' && value.startsWith('0x')) {
-    return '0x' + remove0x(value).padStart(64, '0').toLowerCase()
+    return '0x' + remove0x(value).padEnd(64, '0').toLowerCase()
   } else if (typeof value === 'boolean') {
-    return toHexString32(value ? 1 : 0)
+    return '0x' + `${value ? 1 : 0}`.padStart(64, '0')
   } else {
-    return toHexString32(BigNumber.from(value).toHexString())
+    return (
+      '0x' +
+      remove0x(BigNumber.from(value).toHexString())
+        .padStart(64, '0')
+        .toLowerCase()
+    )
   }
 }
